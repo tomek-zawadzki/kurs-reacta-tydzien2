9a7a116f5f0fe03cbc83eb357816fe9a888d270e
tomek-zawadzki <65092372+tomek-zawadzki@users.noreply.github.com>
2023-02-26 15:26:45 +0100

    fix probelm with start and stop timer

--- timeboxing/src/components/CurrentTimebox.js
+++ timeboxing/src/components/CurrentTimebox.js
@@ -14,7 +14,9 @@ class CurrentTimebox extends React.Component {
     this.handleStart = this.handleStart.bind(this);
     this.handleStop = this.handleStop.bind(this);
     this.togglePause = this.togglePause.bind(this);
+    this.intervalId = null;
   }
+
   handleStart(event) {
     this.setState({
       isRunning: true,
@@ -31,14 +33,17 @@ class CurrentTimebox extends React.Component {
     this.stopTimer();
   }
   startTimer() {
-    this.intervalId = window.setInterval(() => {
-      this.setState((prevState) => ({
-        elapsedTimeInSeconds: prevState.elapsedTimeInSeconds + 0.1,
-      }));
-    }, 100);
+    if (this.intervalId === null) {
+      this.intervalId = window.setInterval(() => {
+        this.setState((prevState) => ({
+          elapsedTimeInSeconds: prevState.elapsedTimeInSeconds + 0.1,
+        }));
+      }, 100);
+    }
   }
   stopTimer() {
     window.clearInterval(this.intervalId);
+    this.intervalId = null;
   }
   togglePause() {
     this.setState(function (prevState) {
